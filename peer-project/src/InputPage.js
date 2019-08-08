10d955bfb5ba1d857548b8d42e587eca6ad33ccf
Michael Taylor <michaelntaylor@gmail.com>
2019-08-08 15:56:56 -0400

    Additions to array see modification of pie chart

---
+++ peer-project/src/InputPage.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class InputPage extends Component {
+  render() {
+    return (
+      <div>
+        <form>
+          <label htmlFor='categories'>Enter number of Categories</label>
+          
+        </form>
+      </div>
+    )
+  }
+}
