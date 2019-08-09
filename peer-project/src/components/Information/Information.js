e8173b9befbd76c8e1aedebbb697beee3a09b0b9
Joseph Kim <josepheh.kim@gmail.com>
2019-08-09 12:10:34 -0400

    setup the dynamic forms

--- peer-project/src/components/Information/Information.js
+++ peer-project/src/components/Information/Information.js
@@ -3,16 +3,24 @@ import ReactDom from 'react-dom';
 import './Information.scss';
 
 class Information extends React.Component {
-  
-  nameAndCategoriesNext(event) {
-    event.preventdefault();
-    console.log("hello world");
+  state = {
+    formStatus: 1
+  }
+  nameAndCategoriesNext = (event) => {
+    event.preventDefault();
+    console.log({
+      title: event.target["chart-title"].value
+    });
+    this.setState({
+      formStatus: 2
+    });
   }
   render() {
-    return (
-      <>
-        <div className="chart-form">
-          <form className="information">
+    let formToRender;
+    switch(this.state.formStatus) {
+      case 1:
+        formToRender = (
+          <div className="information__form1">
             <div className="information__name">
               <label for="chart-title">Enter the Name of the Chart:</label>
               <input type="text" name="chart-title" id="chart-title" required></input>
@@ -21,6 +29,23 @@ class Information extends React.Component {
               <label for="chart-categories">Enter the Number of Categories:</label>
               <input type="text" name="chart-categories" id="chart-categories" required></input>    
             </div>
+          </div>
+        );
+        break;
+      case 2:
+        formToRender = (
+          <div className="information__form2">
+            <p>this is form 2</p>
+          </div>
+        );
+        break;
+    }
+
+    return (
+      <>
+        <div className="chart-form">
+          <form className="information" onSubmit={this.nameAndCategoriesNext}>
+            {formToRender}
             <button className="next-name-categories" type="submit">Next</button>
           </form>
         </div>
